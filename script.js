3dc2cb7e5164cc52301f7136303c971cebcc6067
unknown <falluah@gmail.com>
2021-03-16 21:37:45 +0900

    init project

---
+++ script.js
@@ -0,0 +1,12 @@
+let cards = [
+    {image: './images/main1.jpg', text: 'Peter'},
+    {image: './images/main2.jpg', text: 'Luis'},
+    {image: './images/main3.jpg', text: 'Hank'},
+    {image: './images/main4.jpg', text: 'Douglas'},
+    {image: './images/main5.jpg', text: 'Jesse'},
+    {image: './images/main6.jpg', text: 'Walter'},
+];
+for (i=1; i<=6; i++) {
+    document.querySelector('.card__title__'+i).textContent = cards[i-1].text;
+    document.querySelector('.card__image__'+i).src = cards[i-1].image;
+}
