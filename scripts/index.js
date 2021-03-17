b46a22be2a514f42613bac58e95269d0ba2b43b1
unknown <falluah@gmail.com>
2021-03-18 00:17:56 +0900

    90% popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,16 @@ const popup = document.querySelector('.popup')
 const popupOpenButton = document.querySelector('.js-menu__open-popup')
 const popupCloseButton = document.querySelector('.popup__close')
 const popupSave = document.querySelector('.popup__save')
+const popupCloseByClickOnOverlay = (event) => {
+    console.log({
+        target: event.target,
+        currentTarget: event.currentTarget,
+    })
+    if (event.target != event.currentTarget) {
+        return
+    }
+    popupToggle()
+}
 
 const popupToggle = function (event) {
     console.log("Button clicked")
@@ -11,3 +21,4 @@ const popupToggle = function (event) {
 popupOpenButton.addEventListener('click', popupToggle)
 popupCloseButton.addEventListener('click', popupToggle)
 popupSave.addEventListener('click', popupToggle)
+popup.addEventListener('click', popupCloseByClickOnOverlay)
